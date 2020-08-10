dd3f1dfe4084ef3f79a5170f251e4b8e7c458c4e
unknown <gustta.apolonio@gmail.com>
2020-08-10 01:32:07 -0300

    API no ar...

--- frontend/site/src/services/ListaNegraApi.js
+++ frontend/site/src/services/ListaNegraApi.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:5000'
+    baseURL: 'https://lista-negra-app.herokuapp.com/'
 })
 
 export default class ListaNegraApi {
