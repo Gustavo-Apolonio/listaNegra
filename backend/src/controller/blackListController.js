bfb604748aa5ce8862d51a54e4da6115feb1efbb
Gustavo-Apolonio <gustta.apolonio@gmail.com>
2021-11-05 21:16:49 -0300

    POST - OK

--- backend/src/controller/blackListController.js
+++ backend/src/controller/blackListController.js
@@ -9,6 +9,25 @@ const cnv = new BlackListConverter();
 
 router.post("/create", async (request, response) => {
   try {
+    const itemReq = request.body.item;
+    let item = cnv.toTable(itemReq);
+
+    item = srv.saveItem(item);
+
+    if (item.id_blacklist <= 0)
+      response
+        .status(404)
+        .send(
+          new Error(
+            404,
+            "An error occurred trying to save a new Black List Item."
+          )
+        );
+    else {
+      let itemRes = cnv.toResponse(item);
+
+      response.status(200).send(itemRes);
+    }
   } catch (error) {
     response.status(400).send(new Error(400, error));
   }
