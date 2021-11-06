ff4cea093052ad8a2ebbc2954fd40722a58455ad
Gustavo-Apolonio <gustta.apolonio@gmail.com>
2021-11-05 21:15:37 -0300

    Response models waiting to be called

---
+++ backend/src/models/res/itemRes.js
@@ -0,0 +1,10 @@
+export default class ItemRes {
+  constructor(id, name, reason, place, occurred, lastUpdate) {
+    this.id = id;
+    this.name = name;
+    this.reason = reason;
+    this.place = place;
+    this.occurred = occurred;
+    this.lastUpdate = lastUpdate;
+  }
+}
